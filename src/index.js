15feebf3a95ef1c9c9ae6847a80e6a2427a9dc78
Viczenith <viczenithgodwin@gmail.com>
2025-07-04 19:09:40 +0100

    Prepare for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter } from 'react-router-dom';
+// import App from './App';
+// import './index.css';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <BrowserRouter basename={process.env.PUBLIC_URL || '/'}>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL || '/'}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
